dee85f05b7f3eb4d01e3c6331e432128838d0b6d
Branden <branden.mosley1337@gmail.com>
2019-04-21 11:09:43 -0700

    Create seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+puts "Seeding Users...."
+CSV.read(Rails.root.join('lib', 'seeds', 'thirty_mock_users.csv'), headers: true).each do |row|
+  u = User.create(row.to_hash)
+  puts "#{u.name} saved!"
+end
+
+puts "\nSeeding Restaurants...."
+CSV.read(Rails.root.join('lib', 'seeds', 'one_hundred_mock_restaurants.csv'), headers: true).each do |row|
+  r = Restaurant.create(row.to_hash)
+  puts "#{r.name} saved!"
+end
+
+puts "\nCreating favorites......"
+CSV.read(Rails.root.join('lib', 'seeds', 'one_fifty_favorite_restaurants.csv'), headers: true).each do |row|
+  u = User.find(row['user_id'])
+  r = Restaurant.find(row['restaurant_id'])
+  u.restaurants << r
+  puts "#{u.name} loves #{r.name}!"
+end
+
+puts "\nThere are now #{User.count} rows in the users table"
+puts "There are now #{Restaurant.count} rows in the restaurants table"
+puts "\nFinished!"
