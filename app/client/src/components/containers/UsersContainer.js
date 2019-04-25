214abbcba6f705b6c001b5dfa31c1406b01fef3c
Branden <branden.mosley1337@gmail.com>
2019-04-25 12:34:40 -0700

    Create Sign Up/Login button in UsersContainer

--- app/client/src/components/containers/UsersContainer.js
+++ app/client/src/components/containers/UsersContainer.js
@@ -1,11 +1,36 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  font-size: 0.75em;
+  margin: 0.25em;
+  padding: 0.25em 1em;
+  border: 2px solid white;
+  border-radius: 10px;
+  right: 0;
+  background-color: green;
+  color: white;
+  &:hover {
+    background-color: white;
+    color: green;
+  }
+`;
+
+const StyledContainer = styled.div`
+  position: absolute;
+  width: 25%;
+  right: 0;
+`;
 
 class UsersContainer extends Component {
   // TODO: GET request to check for a logged in user after setting up Devise.
   render() {
     return (
-      <div>Hello from UsersContainer! Put a signup/login button in me!</div>
+      <StyledContainer>
+        <StyledButton>Sign Up</StyledButton>
+        <StyledButton>Login</StyledButton>
+      </StyledContainer>
     );
   }
 }
