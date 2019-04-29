1e246217898de31f52a479379f5f124bd11c8002
Branden <branden.mosley1337@gmail.com>
2019-04-29 12:49:56 -0700

    Begin working on Quiz form

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,30 +1,54 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import styled from 'styled-components';
-
-const QuizStyle = styled.div`
-  border: 2px dotted black;
-`;
 
 class Quiz extends Component {
   render() {
-    const { questions } = this.props;
-    // TODO: This is shit. Fix it.
     return (
-      <QuizStyle>
-        <p>{questions.price.ask}</p>
-        <ul>
-          {questions.price.answers.map(answer => (
-            <li>{answer}</li>
-          ))}
-        </ul>
-        <p>{questions.distance.ask}</p>
-        <ul>
-          {questions.distance.answers.map(answer => (
-            <li>{answer}</li>
-          ))}
-        </ul>
-      </QuizStyle>
+      <div>
+        <form>
+          <p>How much you wanna spend?</p>
+          <label htmlFor="price1">
+            <input type="radio" name="price" id="price1" value="$" />$
+          </label>
+          <br />
+          <label htmlFor="price2">
+            <input type="radio" name="price" id="price2" value="$" />
+            $$
+          </label>
+          <br />
+          <label htmlFor="price3">
+            <input type="radio" name="price" id="price3" value="$" />
+            $$$
+          </label>
+          <br />
+          <label htmlFor="price4">
+            <input type="radio" name="price" id="price4" value="$" />
+            $$$$
+          </label>
+          <br />
+          <p>How far you wanna go?</p>
+          <label htmlFor="distance1">
+            <input type="radio" name="distance" id="distance1" value="$" />
+            &lt;5
+          </label>
+          <br />
+          <label htmlFor="distance2">
+            <input type="radio" name="distance" id="distance2" value="$" />
+            5-10
+          </label>
+          <br />
+          <label htmlFor="distance3">
+            <input type="radio" name="distance" id="distance3" value="$" />
+            10-15
+          </label>
+          <br />
+          <label htmlFor="distance4">
+            <input type="radio" name="distance" id="distance4" value="$" />
+            &gt;15
+          </label>
+          <br />
+        </form>
+      </div>
     );
   }
 }
