5605ec06956ae67fb58eab6e4edee9bda51d00b1
Branden <branden.mosley1337@gmail.com>
2019-05-02 13:44:34 -0700

    Translate radio buttons into RadioGroup component
    This commit does a bunch of stuff I'm really proud of. The RadioGroup component is now abstract and builds collections of radio buttons based on the question it's passed. I moved the questions from QuizContainer and into Quiz during the build, but will probably move them back in a future commit. RadioGroup should also probably be broken out into it's own file.

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,91 +1,35 @@
-/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-
-// What should our data structure look like?
-
-// What do you want to be able to do?
-
-// We need a Radio Button component to generate
-// Radio will need to take in some sort of props to dynamically
-// render data attributes.
-// this.state = {
-//   questions: {
-//     price: {
-//       ask: 'How much do you want to spend?',
-//       answers: ['$', '$$', '$$$', '$$$$'],
-//     },
-//     distance: {
-//       ask: 'How far you wanna go?',
-//       answers: ['5', '10', '15', '20'],
-//     },
-//   },
-// };
-
-// const Radio = props => {
-//   const { qName } = props;
-//   return (
-//     <label htmlFor={`${qName}1`}>
-//       <input type="radio" name={qName} id={`${qName}1`} value="$" />$
-//     </label>
-//   );
-// };
 
 // Each Radio Group is responsible for one question (price, distance)
 // It renders the actual ask and 4 Radio components, 1 for each answer
 // It will take in a prop of one question object, passed to it by Quiz
 
 const RadioGroup = props => {
-  const { handleChange, price } = props;
+  const { handleChange, stateValue, question, name } = props;
+  const radioButtons = question.answers.map((answer, index) => {
+    const id = `${name}${index}`;
+    return (
+      <>
+        <label htmlFor={id}>
+          <input
+            type="radio"
+            id={id}
+            name={name}
+            value={answer}
+            checked={stateValue === answer}
+            onChange={handleChange}
+          />
+          {answer}
+        </label>
+        <br />
+      </>
+    );
+  });
   return (
     <div>
-      <p>How much you wanna spend?</p>
-      <label htmlFor="price1">
-        <input
-          type="radio"
-          id="price1"
-          name="price"
-          value="$"
-          checked={price === '$'}
-          onChange={handleChange}
-        />
-        $
-      </label>
-      <br />
-      <label htmlFor="price2">
-        <input
-          type="radio"
-          id="price2"
-          name="price"
-          value="$$"
-          checked={price === '$$'}
-          onChange={handleChange}
-        />
-        $$
-      </label>
-      <br />
-      <label htmlFor="price3">
-        <input
-          type="radio"
-          id="price3"
-          name="price"
-          value="$$$"
-          checked={price === '$$$'}
-          onChange={handleChange}
-        />
-        $$$
-      </label>
-      <br />
-      <label htmlFor="price4">
-        <input
-          type="radio"
-          id="price4"
-          name="price"
-          value="$$$$"
-          checked={price === '$$$$'}
-          onChange={handleChange}
-        />
-        $$$$
-      </label>
+      <p>{question.ask}</p>
+      {radioButtons}
     </div>
   );
 };
@@ -97,6 +41,16 @@ class Quiz extends Component {
     this.state = {
       price: '',
       distance: '',
+      questions: {
+        price: {
+          ask: 'How much do you want to spend?',
+          answers: ['$', '$$', '$$$', '$$$$'],
+        },
+        distance: {
+          ask: 'How many miles you wanna go?',
+          answers: ['<5', '5-10', '10-15', '>20'],
+        },
+      },
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -115,62 +69,25 @@ class Quiz extends Component {
   }
 
   render() {
-    const { price, distance } = this.state;
+    const { price, distance, questions } = this.state;
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <RadioGroup handleChange={this.handleChange} price={price} />
+          <RadioGroup
+            handleChange={this.handleChange}
+            stateValue={price}
+            question={questions.price}
+            name={Object.keys(questions)[0]}
+          />
 
           <br />
-          <p>How far you wanna go?</p>
-          <label htmlFor="distance1">
-            <input
-              type="radio"
-              name="distance"
-              id="distance1"
-              value="5"
-              checked={distance === '5'}
-              onChange={this.handleChange}
-            />
-            &lt;5
-          </label>
-          <br />
-          <label htmlFor="distance2">
-            <input
-              type="radio"
-              name="distance"
-              id="distance1"
-              value="5-10"
-              checked={distance === '5-10'}
-              onChange={this.handleChange}
-            />
-            5-10
-          </label>
-          <br />
-          <label htmlFor="distance3">
-            <input
-              type="radio"
-              name="distance"
-              id="distance1"
-              value="10-15"
-              checked={distance === '10-15'}
-              onChange={this.handleChange}
-            />
-            10-15
-          </label>
-          <br />
-          <label htmlFor="distance4">
-            <input
-              type="radio"
-              name="distance"
-              id="distance1"
-              value="15"
-              checked={distance === '15'}
-              onChange={this.handleChange}
-            />
-            &gt;15
-          </label>
-          <br />
+
+          <RadioGroup
+            handleChange={this.handleChange}
+            stateValue={distance}
+            question={questions.distance}
+            name={Object.keys(questions)[1]}
+          />
           <button type="submit">CHOOOOOOOSE</button>
         </form>
       </div>
